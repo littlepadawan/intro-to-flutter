60bd90b3f7b0fe256b9260424f1ab5ca00d75915
Ida <34948325+littlepadawan@users.noreply.github.com>
2023-07-30 13:47:41 +0200

    Simple about page done

--- my_app/lib/screens/about.dart
+++ my_app/lib/screens/about.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/widgets/navigation_bar.dart';
+import 'package:flutter/services.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
@@ -10,17 +11,45 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('About'),
       ),
-      body: const Center(
+      body: Padding(
+        padding: EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.stretch, // uncomment to left align text
           children: [
-            Text("Header: You've reached the about page"),
+            SizedBox(height: 30),
+            header(),
             SizedBox(height: 20),
-            Text('Here will be info about the page'),
+            appDescription(),
+            SizedBox(height: 20),
+            author(),
           ],
         ),
       ),
       bottomNavigationBar: const CustomNavigationBar(),
     );
   }
+}
+
+Widget header() {
+  return const Text(
+    'The Weather App',
+    style: TextStyle(
+      fontSize: 22,
+    ),
+  );
+}
+
+Widget appDescription() {
+  return const Text(
+    'This is an app that is developed for the course 1DV535 at Linnaeus University the summer of 2023, using Flutter and the OpenWeatherMap API.',
+    textAlign: TextAlign.center,
+  );
+}
+
+Widget author() {
+  return const Text(
+    'Developed by Ida Hellqvist.',
+    textAlign: TextAlign.left,
+  );
 }
