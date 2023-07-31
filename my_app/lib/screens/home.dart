b875b1d7ed6462f75f43a2350416398d4e5e9028
Ida <34948325+littlepadawan@users.noreply.github.com>
2023-07-31 14:28:59 +0200

    Basic geolocation implemented (WPI)

--- my_app/lib/screens/home.dart
+++ my_app/lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/widgets/navigation_bar.dart';
 import 'package:my_app/providers/fetch_weather_data.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,11 +13,26 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String? fetchedData;
+  Position? currentPosition;
 
   @override
   void initState() {
     super.initState();
+    _getCurrentLocation();
     _fetchData(); // TODO: Geolocator
+  }
+
+  Future<void> _getCurrentLocation() async {
+    PermissionStatus permissionStatus = await Permission.location.request();
+
+    if (permissionStatus.isGranted) {
+      // TODO: Handle if not given permission
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        currentPosition = position;
+      });
+    }
   }
 
   Future<void> _fetchData() async {
@@ -39,20 +56,28 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const SizedBox(height: 30),
-            fetchedData != null
-                ? Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Text(
-                      fetchedData!,
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                  )
-                : Container(),
-          ],
-        ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 30),
+              fetchedData != null
+                  ? Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        fetchedData!,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    )
+                  : Container(),
+              currentPosition != null
+                  ? Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        'Position: $currentPosition',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    )
+                  : Container(),
+            ]),
       ),
       bottomNavigationBar: const CustomNavigationBar(),
     );
