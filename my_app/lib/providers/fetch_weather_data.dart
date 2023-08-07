ad391f605a0634ad0ef82534a68de1f4f23534de
Ida <34948325+littlepadawan@users.noreply.github.com>
2023-08-07 13:14:46 +0200

    Change parameters from string to double

--- my_app/lib/providers/fetch_weather_data.dart
+++ my_app/lib/providers/fetch_weather_data.dart
@@ -3,15 +3,22 @@ import 'dart:convert';
 
 import 'package:my_app/models/weather_data.dart';
 
-Future<WeatherData> fetchWeatherData(String latitude, String longitude) async {
+import '../models/location_data.dart';
+
+Future<WeatherData> fetchWeatherData(double latitude, double longitude) async {
+  String stringLatitude = latitude.toString();
+  String stringLongitude = longitude.toString();
   String url =
-      'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=5430333b2b984d9cf3d533524975465e&units=metric';
+      'https://api.openweathermap.org/data/2.5/weather?lat=$stringLatitude&lon=$stringLongitude&appid=5430333b2b984d9cf3d533524975465e&units=metric';
   final response = await http.get(Uri.parse(url));
 
   if (response.statusCode != 200) {
     throw Exception('Failed to load JSON data');
   }
 
-  WeatherData weatherData = WeatherData.fromJson(json.decode(response.body));
+  final locationData =
+      await ReverseGeocodingService().getGeolocationData(latitude, longitude);
+  WeatherData weatherData = WeatherData.fromJson(
+      json.decode(response.body), latitude, longitude, locationData);
   return weatherData;
 }
